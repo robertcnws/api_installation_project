1db9ef4a6923a0f3b8547d45a7febb5558a04021
robertocnws <robertoc@newwindowsystem.com>
2025-08-22 11:27:49 -0400

    feat: Update Dockerfiles to use MongoDB 7.0 and improve installation commands for better efficiency

--- frontend_app/src/auth/context/data/hooks/use-filtered-measurements.jsx
+++ frontend_app/src/auth/context/data/hooks/use-filtered-measurements.jsx
@@ -6,8 +6,11 @@ export function useFilteredMeasurements(measurements, projects, services, userLo
   let finalMeasurements = measurements;
   if (userLogged?.data?.user_role?.name.toLowerCase().includes(CONFIG.roles.installer.toLowerCase())) {
     finalMeasurements = finalMeasurements?.filter(m => (
-      projects.some(project => project?.id === m?.project?.id) || 
-      services.some(service => service?.id === m?.service?.id) 
+      // projects.some(project => project?.id === m?.project?.id) || 
+      // services.some(service => service?.id === m?.service?.id) 
+      m?.userManager?.username === userLogged?.data?.username ||
+      m?.firstAssignee?.username === userLogged?.data?.username ||
+      m?.checkAssignee?.username === userLogged?.data?.username
     ));
   }
   return useMemo(() => finalMeasurements, [finalMeasurements]);
